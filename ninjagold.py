9e7ee8d03bf5968c1170fb0918d691cd1b81e33d
Troy Center <troycenter1@gmail.com>
2017-06-11 14:12:44 -0700

    finished core web page for game...

---
+++ ninjagold.py
@@ -0,0 +1,23 @@
+'''Ninja Gold Assignment 
+by: Troy Center, troycenter1@gmail.com, Coding Dojo Python fundamentals, June 2017
+'''
+
+#pylint: disable=C0103,C0111
+
+from flask import Flask, render_template, redirect, session
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainpage():
+    return render_template('ninjagold.html')
+
+
+
+@app.route('/process_money')
+def processmoney():
+    return redirect('/')
+
+
+
+app.run(debug=True)
