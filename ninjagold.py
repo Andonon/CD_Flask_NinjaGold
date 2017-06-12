2c3434a1e7d63fb6d5f61de7cb6b2788e137079e
Troy Center <troycenter1@gmail.com>
2017-06-12 08:56:49 -0700

    finsihed output to page... styling is all that's needed now. Submitted assignment

--- ninjagold.py
+++ ninjagold.py
@@ -1,69 +1,67 @@
 '''Ninja Gold Assignment
 by: Troy Center, troycenter1@gmail.com, Coding Dojo Python fundamentals, June 2017
 '''
+import datetime
+import random
+from flask import Flask, render_template, redirect, session, request
 
 #pylint: disable=C0103,C0111
 
-from flask import Flask, render_template, redirect, session, request
-
 app = Flask(__name__)
 app.secret_key = 'kljasldjlaskjdlkasjdlkjasdlkj'
-
-def getrandomnum(start,end):
-    import random
-    return random.randint(start, end)
-
-def hasgold():
-    #check to see if they have any gold, set variable.
-    try:
-        session['gold'] += 0
-        print "=== got session gold from client ==="
-    except:
-        session['gold'] = 0
-        print "=== no session gold, set to 0 ==="
-        return
-
-
 @app.route('/')
 def mainpage():
+
+    try:
+        session['gold']
+    except KeyError:
+        session['gold'] = 0
+    try:
+        session['activities']
+    except KeyError:
+        session['activities'] = []
+
     return render_template('ninjagold.html')
 
 
 
 @app.route('/process_money', methods=['post'])
 def processmoney():
-    print "35=== Got Form Post: ===", request.form
-    #check if they have any gold
-    hasgold()
-    currgold = session['gold']
-    print "39===Gold was ", session['gold']
+
     if request.form.get('action') == "farm":
-        #add 10 to 20 gold
-        print "42=== Detected farm activity ==="
-        earnings = getrandomnum(10, 20)
-        currgold = currgold + earnings
+        earnings = random.randrange(10, 20)
+        session['gold'] += earnings
+        session['activities'].append(
+            'Earned ' + str(earnings) + ' gold from the farm! (' +
+            '{:%Y/%m/%d %I:%M %p})'.format(datetime.datetime.now())
+        )
     elif request.form.get('action') == "cave":
-        #add 5 to 10 fold
-        print "47=== Detected cave activity ==="
-        earnings = getrandomnum(5, 10)
-        currgold = currgold + earnings
+        earnings = random.randrange(5, 10)
+        session['gold'] += earnings
+        session['activities'].append(
+            'Earned ' + str(earnings) + ' gold from the cave! (' +
+            '{:%Y/%m/%d %I:%M %p})'.format(datetime.datetime.now())
+        )
     elif request.form.get('action') == "house":
-        #add 2 to 5 gold
-        print "52=== Detected house activity ==="
-        earnings = getrandomnum(2, 5)
-        currgold = currgold +  earnings
+        earnings = random.randrange(2, 5)
+        session['gold'] += earnings
+        session['activities'].append(
+            'Earned ' + str(earnings) + ' gold from the house! (' +
+            '{:%Y/%m/%d %I:%M %p})'.format(datetime.datetime.now())
+        )
     elif request.form.get('action') == "casino":
-        #add -50 to 50 gold
-        print "57=== Detected casino activity ==="
-        earnings = getrandomnum(-50, 50)
-        currgold = currgold +  earnings
-    else:
-        pass
-    print "You earned ", earnings
-    print "Gold is now", currgold
-    session['gold'] = currgold
+        earnings = random.randrange(-50, 50)
+        session['gold'] += earnings
+        if earnings > 0:
+            session['activities'].append(
+                'Entered a casino and Won ' + str(earnings) + ' gold! Ouch! (' +
+                '{:%Y/%m/%d %I:%M %p})'.format(datetime.datetime.now())
+            )
+        else:
+            session['activities'].append(
+                'Entered a casino and Lost ' + str(earnings) + ' gold. Holy Cow! (' +
+                '{:%Y/%m/%d %I:%M %p})'.format(datetime.datetime.now())
+            )
     return redirect('/')
-
-
 
 app.run(debug=True)
